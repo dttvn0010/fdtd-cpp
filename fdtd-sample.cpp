1c58f4b5dc9dc2481244e9c8638c016b2e521ddd
tungdt86 <tungdt86@fpt.com>
2024-08-12 12:20:53 +0700

    Update

--- fdtd-sample.cpp
+++ fdtd-sample.cpp
@@ -6,6 +6,7 @@
 
 int main()
 {
+    // Initialize a new grid for simulation
     Grid grid(
         Tuple<double, double, double>(2.5e-5, 1.5e-5, 0),
         0.1 * WAVELENGTH,
@@ -15,6 +16,7 @@ int main()
 
     auto all = Slice::all();
 
+    // Add x-low boundary
     grid.add_boundary(
         new PML("pml_xlow"),
         Slice(0,10),
@@ -22,6 +24,7 @@ int main()
         all
     );
 
+    // Add x-high boundary
     grid.add_boundary(
         new PML("pml_xhigh"),
         Slice::tail(-10),
@@ -29,6 +32,7 @@ int main()
         all
     );
 
+    // Add y-low boundary
     grid.add_boundary(
         new PML("pml_ylow"),
         all,
@@ -36,6 +40,7 @@ int main()
         all
     );
 
+    // Add y-high boundary
     grid.add_boundary(
         new PML("pml_yhigh"),
         all,
@@ -43,6 +48,7 @@ int main()
         all
     );
 
+    // Add z boundary
     grid.add_boundary(
         new PeriodicBoundary("zbounds"),
         all,
@@ -50,6 +56,7 @@ int main()
         Slice(0, 0)
     );
 
+    // Add a line source
     grid.add_source(
         new LineSource("linesource", WAVELENGTH / SPEED_LIGHT),
         Slice(50,55),
@@ -57,6 +64,7 @@ int main()
         Slice(0, 0)
     );
 
+    // Add a point source
     grid.add_source(
         new PointSource("pointsource", WAVELENGTH / SPEED_LIGHT),
         Slice(100, 100),
@@ -64,6 +72,7 @@ int main()
         Slice(0, 0)
     );
 
+    // Add a detector
     grid.add_detector(
         new LineDetector("detector"),
         Slice(77,77),
@@ -71,14 +80,17 @@ int main()
         Slice(0, 0)
     );
 
+    // Add an anisotropic obstacle
     grid.add_obstacle(new AnisotropicObstacle("object", 2.5),
         Slice(11, 32),
         Slice(30,84),
         Slice(0, 1)
     );
 
+    // Run the simulation
     grid.run(50);
 
+    // Collect electric & magnetic strength
     auto Ez = grid.E.get(all, all, 0, 2);
     auto Hx = grid.H.get(all, all, 0, 0);
     auto Hy = grid.H.get(all, all, 0, 1);
@@ -86,5 +98,6 @@ int main()
     print("Ez=", Ez);
     print("Hx=", Hx);
     print("Hy=", Hy);
+
     return  0;
 }
